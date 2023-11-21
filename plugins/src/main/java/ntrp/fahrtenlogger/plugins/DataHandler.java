50bd2c5c1762135a290174fc8f47d91b98537e74
robin13901 <robin13901@gmail.com>
2023-11-21 15:00:07 +0100

    added Trips

--- plugins/src/main/java/ntrp/fahrtenlogger/plugins/DataHandler.java
+++ plugins/src/main/java/ntrp/fahrtenlogger/plugins/DataHandler.java
@@ -19,22 +19,49 @@ import com.opencsv.bean.StatefulBeanToCsvBuilder;
 import com.opencsv.exceptions.CsvDataTypeMismatchException;
 import com.opencsv.exceptions.CsvException;
 import com.opencsv.exceptions.CsvRequiredFieldEmptyException;
+import ntrp.fahrtenlogger.domain.Entities.Trip;
 import ntrp.fahrtenlogger.domain.Refuelling;
 
 public class DataHandler {
-    private Path fuel_data_path = Paths.get("plugins/src/main/resources/FUEL_DATA.csv");
-    private Path car_data_path = Paths.get("plugins/src/main/resources/CAR_DATA.csv");
 
-    public DataHandler(Path fuel_data_path, Path car_data_path) {
-        this.fuel_data_path = fuel_data_path;
-        this.car_data_path = car_data_path;
+    private Path fuelDataPath = Paths.get("plugins/src/main/resources/FUEL_DATA.csv");
+    private Path carDataPath = Paths.get("plugins/src/main/resources/CAR_DATA.csv");
+
+    private static final Path TRIPS_PATH = Paths.get("plugins/src/main/resources/trips.csv");
+
+
+    public DataHandler(Path fuelDataPath, Path carDataPath) {
+        this.fuelDataPath = fuelDataPath;
+        this.carDataPath = carDataPath;
     }
 
-    public DataHandler() { }
+    public DataHandler() {}
+
+    public List<Trip> getAllTrips() {
+        List<Trip> trips = new ArrayList<>();
+        try (
+                Reader reader = Files.newBufferedReader(TRIPS_PATH);
+                CSVReader csvReader = new CSVReader(reader)
+        ) {
+            csvReader.readAll().stream().skip(1).map(line -> {
+                String[] attributes = line[0].split(";");
+                return new Trip(
+                        Integer.parseInt(attributes[0]),
+                        Integer.parseInt(attributes[1]),
+                        attributes[2],
+                        attributes[3],
+                        Float.parseFloat(attributes[4])
+                );
+            }).forEach(trips::add);
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+        return trips;
+    }
 
     public List<Refuelling> readFuelData() {
-        List<Refuelling> refuellings = new ArrayList<Refuelling>();
-        try (Reader reader = Files.newBufferedReader(this.fuel_data_path)) {
+        List<Refuelling> refuellings = new ArrayList<>();
+        try (Reader reader = Files.newBufferedReader(this.fuelDataPath)) {
             try (CSVReader csvReader = new CSVReader(reader)) {
                 for (String[] fuelRecord : csvReader.readAll()) {
 
@@ -48,10 +75,7 @@ public class DataHandler {
     }
 
     public List<FuelRecordBean> beanBuilder(Class<FuelRecordBean> c) throws Exception {
-        try (Reader reader = Files.newBufferedReader(fuel_data_path)) {
-            //            for (FuelRecordBean b : cb) {
-//                out.add(new Refuelling(b.getId(), b.getFuelType(), b.getAmount()));
-//            }
+        try (Reader reader = Files.newBufferedReader(fuelDataPath)) {
             CsvToBean<FuelRecordBean> cb = new CsvToBeanBuilder<FuelRecordBean>(reader)
                     .withType(c)
                     .build();
@@ -59,8 +83,17 @@ public class DataHandler {
         }
     }
 
+    public List<TripBean> buildBean(Class<TripBean> c) throws Exception {
+        try (Reader reader = Files.newBufferedReader(TRIPS_PATH)) {
+            CsvToBean<TripBean> tripBean = new CsvToBeanBuilder<TripBean>(reader)
+                    .withType(c)
+                    .build();
+            return tripBean.parse().stream().toList();
+        }
+    }
+
     public void beanWriter(List<Refuelling> data) throws IOException, CsvRequiredFieldEmptyException, CsvDataTypeMismatchException {
-        try (Writer writer = new FileWriter(String.valueOf(fuel_data_path))) {
+        try (Writer writer = new FileWriter(String.valueOf(fuelDataPath))) {
             StatefulBeanToCsv<Refuelling> sbc = new StatefulBeanToCsvBuilder<Refuelling>(writer)
                     .withQuotechar('\'')
                     .withSeparator(CSVWriter.DEFAULT_SEPARATOR)
