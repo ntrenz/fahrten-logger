26b8c543ab5302f6d67f9c5d2f95fae2231ce5d5
ntrenz <niklas@gt-trenz.de>
2023-11-27 23:19:44 +0100

    implement TripInterpreter and test class

--- adapters/src/main/java/ntrp/fahrtenlogger/adapters/interpreter/TripInterpreter.java
+++ adapters/src/main/java/ntrp/fahrtenlogger/adapters/interpreter/TripInterpreter.java
@@ -1,24 +1,81 @@
 package ntrp.fahrtenlogger.adapters.interpreter;
 
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
 import java.util.List;
+import java.util.Locale;
 
-public class TripInterpreter implements CommandInterpreter {
-    private final List<String> commands;
-    private Actions action;
+import ntrp.fahrtenlogger.domain.Entities.Place;
+import ntrp.fahrtenlogger.domain.ValueObjects.Kilometer;
+
+public class TripInterpreter extends CommandInterpreter {
+    private final int num_of_mandatory_arguments = 3;
+    private Place from_place;
+    private Place to_place;
+    private Kilometer distance = new Kilometer(0);
+    private LocalDate date = LocalDate.now();
 
     public TripInterpreter(List<String> args) {
-        System.out.println("My arguments are:");
-        args.forEach(System.out::println);
-        this.commands = args;
+        super(args);
+    }
+
+    public Place getFrom_place() {
+        return from_place;
+    }
+
+    public Place getTo_place() {
+        return to_place;
+    }
+
+    public Kilometer getDistance() {
+        return distance;
+    }
+
+    public LocalDate getDate() {
+        return date;
     }
 
     @Override
-    public void interpretCommands() {
+    public void parseCommands() {
+        // command-structure:
+        // trip <new:modify:delete> <from> <to> <-di <distance:?>> <-d <date:?>>
+        // TODO: Error-Behandlung
+        if (arguments_list.size() < num_of_mandatory_arguments)
+            throw new IllegalArgumentException("Nicht genügend Parameter!");
+        parseAction(arguments_list.get(0));
+        this.from_place = new Place(arguments_list.get(1));
+        this.to_place = new Place(arguments_list.get(2));
 
+        int index = num_of_mandatory_arguments;
+        while (arguments_list.size() > index) {
+            parseOptionalArguments(index);
+            index += 2;
+        }
+    }
+
+    private void parseOptionalArguments(int index) {
+        if (arguments_list.get(index).equals("-d"))
+            this.date = LocalDate.parse(arguments_list.get(++ index), DateTimeFormatter.ofPattern("dd.MM.yyyy", Locale.GERMAN));
+        else if (arguments_list.get(index).equals("-di"))
+            this.distance = new Kilometer(Double.parseDouble(arguments_list.get(++ index)));
+    }
+
+    @Override
+    public String toString() {
+        return "TripInterpreter{from_place=" + from_place +
+                ", to_place=" + to_place +
+                ", distance=" + distance +
+                ", date=" + date +
+                ", action=" + action +
+                '}';
     }
 
     @Override
     public void executeCommands() {
+        System.out.println(this);
+    }
 
+    public static String getHelp() {
+        return "TRIP: creates, updates or deletes a trip. A trip is a traveled distance between two places.\n---- arguments:\n\t<new> <from> <to> <-di <distance:?>> <-d <date:?>>\n\t<modify>\n\t<delete>\n----";
     }
 }
---
+++ adapters/src/test/java/ntrp/fahrtenlogger/adapters/interpreter/TripInterpreterTest.java
@@ -0,0 +1,63 @@
+package ntrp.fahrtenlogger.adapters.interpreter;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import java.time.LocalDate;
+import java.time.format.DateTimeParseException;
+import java.util.List;
+
+class TripInterpreterTest {
+    TripInterpreter tripInterpreter;
+
+    @BeforeEach
+    void setUp() {
+        this.tripInterpreter = new TripInterpreter(List.of("new", "a", "b", "-d", "22.11.2023", "-di", "55.3"));
+    }
+
+    @AfterEach
+    void tearDown() {
+    }
+
+    @Test
+    void interpretCommandsTooFewArguments() {
+        TripInterpreter tI = new TripInterpreter(List.of("new", "a"));
+
+        assertThrows(IllegalArgumentException.class, () -> tI.parseCommands());
+    }
+
+    @Test
+    void interpretCommandsCorrectWith3() {
+        tripInterpreter.parseCommands();
+
+        assertEquals(tripInterpreter.getDistance().getValue(), 55.3);
+        // assertEquals(tripInterpreter.getFrom_place(), new Place("a"));
+        // assertEquals(tripInterpreter.getTo_place(), new Place("b"));
+        assertEquals(tripInterpreter.getDate(), LocalDate.of(2023, 11, 22));
+    }
+
+    @Test
+    void interpretCommandsWithWrongDate() {
+        TripInterpreter tI = new TripInterpreter(List.of("new", "a", "b", "-d", "HALLO"));
+
+        assertThrows(DateTimeParseException.class, tI::parseCommands);
+    }
+
+    @Test
+    void parseOptionalArguments() {
+        // int index = 3;
+        // this.tripInterpreter.parseOptionalArguments(index);
+    }
+
+    @Test
+    void executeCommands() {
+    }
+
+    @Test
+    void correctHelp() {
+        assertEquals(TripInterpreter.getHelp(), "TRIP: creates, updates or deletes a trip. A trip is a traveled distance between two places.\n---- arguments:\n\t<new> <from> <to> <-di <distance:?>> <-d <date:?>>\n\t<modify>\n\t<delete>\n----");
+    }
+}
