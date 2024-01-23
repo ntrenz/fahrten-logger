9673dbcdcdcffa10664b42eff5ea80046102dc0d
ntrenz <niklas@gt-trenz.de>
2024-01-23 17:45:13 +0100

    example data for fuel

--- plugins/src/main/java/module-info.java
+++ plugins/src/main/java/module-info.java
@@ -2,6 +2,7 @@ module plugins {
     requires adapters;
     requires domain;
     requires com.opencsv;
+    requires java.sql;
     exports ntrp.fahrtenlogger.plugins;
     exports ntrp.fahrtenlogger.plugins.converter;
 }
