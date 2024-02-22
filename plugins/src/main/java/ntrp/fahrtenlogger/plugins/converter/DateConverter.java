b9ce4fcd7d796e5b3d4b310b71e89d38af3a18c3
ntrenz <niklas@gt-trenz.de>
2024-02-22 09:36:44 +0100

    improve Bean converter

--- plugins/src/main/java/ntrp/fahrtenlogger/plugins/converter/DateConverter.java
+++ plugins/src/main/java/ntrp/fahrtenlogger/plugins/converter/DateConverter.java
@@ -1,8 +1,8 @@
 package ntrp.fahrtenlogger.plugins.converter;
 
 import com.opencsv.bean.AbstractBeanField;
-import com.opencsv.exceptions.CsvConstraintViolationException;
 import com.opencsv.exceptions.CsvDataTypeMismatchException;
+
 import ntrp.fahrtenlogger.plugins.FuelRecordBean;
 
 import java.time.LocalDate;
