e4433d364c4a0d86b7a491385c6d33ad2258d31f
ntrenz <niklas@gt-trenz.de>
2024-03-04 17:43:06 +0100

    make CSVBean static and add Header Line Attribute for CSV Files

--- plugins/src/main/java/ntrp/fahrtenlogger/plugins/CsvBean.java
+++ plugins/src/main/java/ntrp/fahrtenlogger/plugins/CsvBean.java
@@ -1,12 +1,8 @@
 package ntrp.fahrtenlogger.plugins;
 
 import java.nio.file.Path;
-import java.nio.file.Paths;
 
-public class CsvBean {
-    static Path path = Paths.get("plugins/src/main/resources/FUEL_DATA.csv");
-
-    public static Path getPath() {
-        return path;
-    }
+public abstract class CsvBean {
+    public abstract Path getPath();
+    public abstract String getHeaderLine();
 }
