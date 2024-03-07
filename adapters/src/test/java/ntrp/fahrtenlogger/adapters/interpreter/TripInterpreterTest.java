101817589a4a00fd87aa11d1a3209508117a7f5d
ntrenz <niklas@gt-trenz.de>
2024-03-07 18:19:19 +0100

    test read parser method

--- adapters/src/test/java/ntrp/fahrtenlogger/adapters/interpreter/TripInterpreterTest.java
+++ adapters/src/test/java/ntrp/fahrtenlogger/adapters/interpreter/TripInterpreterTest.java
@@ -208,12 +208,84 @@ class TripInterpreterTest {
     
     @Test
     void parseCommandRead() {
+        Actions action = Actions.READ;
+        List<String> commandsList = new ArrayList<>();
+        commandsList.add(action.toString().toLowerCase());
 
+        tripInterpreter = new TripInterpreter(commandsList, null);
+
+        tripInterpreter.parseCommands();
+        assertEquals(Actions.READ, tripInterpreter.action);
+        assertNull(tripInterpreter.getDate());
+        assertEquals(0, tripInterpreter.getId());
     }
 
     @Test
     void parseCommandReadWithArgs() {
+        Actions action = Actions.READ;
+        Place fromPlace = new Place("FROM_PLACE");
+        Place toPlace = new Place("TO_PLACE");
+        String dateString = "01.03.2024";
+        LocalDate date = LocalDate.parse(dateString, DateTimeFormatter.ofPattern("dd.MM.yyyy", Locale.GERMAN));
+        Kilometer distance = new Kilometer(5);
 
+        List<String> commandsList = new ArrayList<>();
+        commandsList.add(action.toString().toLowerCase());
+        commandsList.add("-fp");
+        commandsList.add(fromPlace.toString());
+        commandsList.add("-tp");
+        commandsList.add(toPlace.toString());
+        commandsList.add("-d");
+        commandsList.add(dateString);
+        commandsList.add("-di");
+        commandsList.add(distance.toString());
+
+        tripInterpreter = new TripInterpreter(commandsList, null);
+
+        tripInterpreter.parseCommands();
+        assertEquals(Actions.READ, tripInterpreter.action);
+        assertNull(tripInterpreter.getDate());
+        assertEquals(0, tripInterpreter.getId());
+        assertEquals(fromPlace, tripInterpreter.getFromPlace());
+        assertEquals(toPlace, tripInterpreter.getToPlace());
+        assertEquals(date, tripInterpreter.getDate());
+        assertEquals(distance, tripInterpreter.getDistance());
+    }
+
+    @Test
+    void parseCommandReadWithWrongDateFormat() {
+        Actions action = Actions.READ;
+        String dateString = "01-03-2024";
+
+        List<String> commandsList = new ArrayList<>();
+        commandsList.add(action.toString().toLowerCase());
+        commandsList.add("-d");
+        commandsList.add(dateString);
+
+        tripInterpreter = new TripInterpreter(commandsList, null);
+
+        Throwable exception = assertThrows(IllegalArgumentException.class, () -> {
+            tripInterpreter.parseCommands();
+        });
+        assertEquals("Date could not be parsed! (Date format must be: DD.MM.YYYY)", exception.getMessage());
+    }
+
+    @Test
+    void parseCommandReadWithWrongDistanceFormat() {
+        Actions action = Actions.READ;
+        String distance = "5km";
+
+        List<String> commandsList = new ArrayList<>();
+        commandsList.add(action.toString().toLowerCase());
+        commandsList.add("-di");
+        commandsList.add(distance);
+
+        tripInterpreter = new TripInterpreter(commandsList, null);
+
+        Throwable exception = assertThrows(IllegalArgumentException.class, () -> {
+            tripInterpreter.parseCommands();
+        });
+        assertEquals("Distance must be a number! (Number format for Distances is: X.X)", exception.getMessage());
     }
 
     @Test
